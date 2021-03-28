8519d0b8ed5570a71c069d8853c57507dd6cf584
beam-beam <613170010108@rmu.ac.th>
2021-03-28 16:01:20 +0700

    create pingcontroller

---
+++ src/main/java/com/it/ApartmantApiApplication.java
@@ -0,0 +1,13 @@
+package com.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmantApiApplication.class, args);
+	}
+
+}
